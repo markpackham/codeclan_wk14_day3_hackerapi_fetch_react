1e7d358f1a71982453cddd266e6a3c0e6b184d73
Robin Clark <robindtclark@gmail.com>
2020-07-08 10:35:13 +0100

    add skeleton app with base components

---
+++ src/containers/NewsContainer.js
@@ -0,0 +1,20 @@
+import React, { Component } from 'react'
+
+class NewsContainer extends Component{
+
+    constructor(props){
+        super(props);
+        this.state = {};
+    }
+
+    componentDidMount(){
+    }
+
+    render(){
+        return(
+            <h1>This is a test.</h1>
+        )
+    }
+}
+
+export default NewsContainer;
