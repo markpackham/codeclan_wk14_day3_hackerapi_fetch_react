9df0f99f50230ef8ac8405a74d1e931a98e2c4f1
Robin Clark <robindtclark@gmail.com>
2020-07-08 13:46:24 +0100

    Add key to our list

--- src/components/NewsList.js
+++ src/components/NewsList.js
@@ -2,10 +2,26 @@ import React from 'react';
 import NewsDetail from './NewsDetail';
 
 const NewsList = (props) => {
+        const newsStories = props.stories.map(story => {
+        return (
+            <div key={story.id} className="news-card">
+                <div>
+                    Title: {story.title}
+                </div>
+                <div>
+                    Author: {story.by}
+                </div>
+                <div>
+                    Score: {story.score}
+                </div>
+                <a target="_blank" href={story.url}>link</a>
+            </div>
+        )
+    })
 
     return(
         <div className="news-list">
-
+            {newsStories}
         </div>
     )
     
--- src/containers/NewsContainer.js
+++ src/containers/NewsContainer.js
@@ -18,20 +18,21 @@ class NewsContainer extends Component{
       fetch('https://hacker-news.firebaseio.com/v0/topstories.json')
        .then(res => res.json())
        .then(data => {
-        const promises = data.slice(0,10).map((storyId) => {
+        const promises = data.slice(0,20).map((storyId) => {
           return (
             fetch(`https://hacker-news.firebaseio.com/v0/item/${storyId}.json`)
              .then(res => res.json())
           )
         })
-        Promise.resolve(promises)
-         .then(stories => this.setState({stories: stories}))
+        Promise.all(promises)
+         .then(data => this.setState({stories: data}))
        })
     }
 
     render(){
         return(
             <div className="news-container">
+                <h1>Top Stories</h1>
             <NewsList stories={this.state.stories}/>
             </div>
         )
